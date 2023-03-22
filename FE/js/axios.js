2bf15e2539a38bb7c2f79b1218318f1ce787e6aa
Ivan <102649768+Iaxity@users.noreply.github.com>
2023-03-22 11:17:36 +0000

    master: FE setiing

--- FE/js/axios.js
+++ FE/js/axios.js
@@ -1,21 +1,10 @@
-import { boot } from "quasar/wrappers";
-import axios from "axios";
+import axios from 'axios';
+import 'regenerator-runtime/runtime';
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
 const api = axios.create({
   // baseURL: "https://iaxity-ideal-space-orbit-jpwv6gwp76jfpgwg-6544.preview.app.github.dev/",
   baseURL: "http://localhost:6544",
   // baseURL: "https://andy-dry-glorious-disco-9r4rwg9v5g4h9pg5-6544.preview.app.github.dev/"
-});
-
-api.interceptors.request.use((config) => {
-  config.headers["ngrok-skip-browser-warning"] = true;
-  return config;
 });
 
 export default boot(({ app }) => {
