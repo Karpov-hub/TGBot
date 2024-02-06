503177681ebefe397d06895470f062ed4671a66b
Mihail <mk8385873@gmail.com>
2024-02-06 15:02:12 +0300

    update package

--- BE/app.js
+++ BE/app.js
@@ -1,18 +1,29 @@
-const bodyParser = require("body-parser");
+const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+
+// Убедитесь, что у вас есть токен вашего бота
+const token = "6883415167:AAFQNSnFL-s73ORXuKuRnjRqlZw0Q7APzGY";
+const bot = new TelegramBot(token, { polling: true });
 
 app.use(bodyParser.json({ limit: "8mb" }));
 app.use(bodyParser.urlencoded({ limit: "8mb", extended: true }));
 
+// Обработка запросов от телеграма
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
-const userRouter = require("../BE/routers/userRouter");
-app.use("/user", userRouter);
-
-const authRouter = require("../BE/routers/authRouter");
-app.use("/auth", authRouter);
-
-app.listen(port, () => {
+// Запуск Express сервера
+const server = app.listen(port, () => {
   console.log(`App listen port: http://localhost:${port}`);
+});
+
+// Обработка ошибок
+server.on("error", (error) => {
+  console.error("Server error:", error);
 });
