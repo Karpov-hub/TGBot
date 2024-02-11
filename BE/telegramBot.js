5df55b8bcb68b7643f450d992c80624b43fac95c
Mihail <mk8385873@gmail.com>
2024-02-11 06:08:16 +0300

    4

--- BE/telegramBot.js
+++ BE/telegramBot.js
@@ -1,7 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const db = require("./db/models/index");
 const { Op, where } = require("sequelize");
-// Убедитесь, что у вас есть токен вашего бота
 const token = "6883415167:AAFQNSnFL-s73ORXuKuRnjRqlZw0Q7APzGY";
 const bot = new TelegramBot(token, { polling: true });
 
@@ -34,21 +33,25 @@ async function updateOrder(order_id, updatingOptions) {
         id: order_id,
       },
     });
-    return res.send({ success: true });
+    return true;
   } catch (err) {
-    return res.send({ success: false, err });
+    return false;
   }
 }
 
 async function sendOrderToMaster(chatId, name, surname, orderData) {
   try {
     // Формируем текст сообщения с данными заказа
-    const message = `Адрес: ${orderData.address}\nТелефон клиента: ${orderData.client_phone}\nВремя встречи: ${orderData.meeting_time}\nМарка: ${orderData.brand}\nТип техники: ${orderData.product_type}\nМастер: ${surname} ${name}`;
+    const message = `Адрес: ${orderData.address}
+    \nТелефон клиента: ${orderData.client_phone}
+    \nВремя встречи: ${orderData.meeting_time}
+    \nМарка: ${orderData.brand}\nТип техники: ${orderData.product_type}
+    \nМастер: ${surname} ${name}`;
 
     // Создаем кнопку "Принять"
     const acceptButton = {
       text: "Принять",
-      callback_data: "accept_order", // Данные, которые будут отправлены обратно боту при нажатии на кнопку
+      callback_data: `accept_order_${orderData.id}`, // Данные, которые будут отправлены обратно боту при нажатии на кнопку
     };
 
     // Формируем клавиатуру с кнопкой
@@ -71,18 +74,40 @@ bot.on("callback_query", async (callbackQuery) => {
   const data = callbackQuery.data;
 
   // Проверяем, что нажата кнопка "Принять"
-  if (data === "accept_order") {
-    const updatingOptions = {
+  if (data.startsWith("accept_order_")) {
+    const orderId = data.split("_")[2]; // Получаем идентификатор заказа из callback_data
+
+    // Вызываем функцию обновления заказа
+    await updateOrder(orderId, {
       confirmed_at: new Date(),
-    };
-    const updateOrder = await updateOrder(order_id, updatingOptions);
+    });
 
-    // Отправляем сообщение об успешном принятии заказа
-    bot.sendMessage(chatId, "Заказ успешно принят!");
+    // Получаем данные о заказе из базы данных
+    const orderData = await db.order.findOne({
+      where: {
+        id: orderId,
+      },
+    });
+    const masterData = await db.user.findOne({
+      where: {
+        id: orderData.assigned_master,
+      },
+      attributes: ["name", "surname"],
+    });
+    // Отправляем сообщение мастеру о принятии заказа с данными из заказа
+    const messageToMaster = `Адрес: ${orderData.address}
+    \nТелефон клиента: ${orderData.client_phone}
+    \nВремя встречи: ${orderData.meeting_time}
+    \nМарка: ${orderData.brand}
+    \nТип техники: ${orderData.product_type}
+    \nПринял: ${orderData.confirmed_at.toLocaleString()}
+    \nМастер: ${masterData.surname} ${masterData.name}`;
+
+    bot.sendMessage(chatId, messageToMaster);
+
+    // Удаляем инлайн-клавиатуру после нажатия на кнопку
+    bot.deleteMessage(chatId, messageId);
   }
-
-  // Удаляем инлайн-клавиатуру после нажатия на кнопку
-  bot.deleteMessage(chatId, messageId);
 });
 
 // Обработчик команды /start - регистрация нового мастера
